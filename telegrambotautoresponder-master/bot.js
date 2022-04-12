7a945d18745ae96cd301b481a22885843fcc69b1
Ahmed Othman <a7mad.othman95@gmail.com>
2022-04-13 01:35:12 +0200

    Update bot.js

--- telegrambotautoresponder-master/bot.js
+++ telegrambotautoresponder-master/bot.js
@@ -14,74 +14,12 @@ console.log = function (d) { //
 };
 // ---------- LOGGIN FEATURES END ----------
 
-// ---------- FAST ANSWERS ----------
-var fastAnswers = JSON.parse(fs.readFileSync('./answers.json', 'utf8'));
-// ---------- FAST ANSWERS END ----------
-
 console.log("[DEBUG] Bot is starting...");
 
 
 bot.on('message', (msg) => {
-	//bot.sendMessage(msg.chat.id, "Hello dear user"); 
-	//console.log(msg);
-	let date = new Date(msg.date * 1000);
-	let timestamp = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + "@" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-
-
-	let msgFromInfo = "";
-	if (msg.chat.type == "private") {
-		msgFromInfo = msg.from.first_name + "(" + msg.from.id + ")";
-	} else if (msg.chat.type == "group") {
-		msgFromInfo = msg.from.first_name + "(" + msg.from.id + "/" + msg.chat.title + ")";
-	}
-
-	console.log("[INFO](" + timestamp + ") Msg from " + msgFromInfo + ": " + msg.text);
-
 	if (msg.text != null) {
-		let mex = controlMessage(msg.text);
-		if (mex != null) {
-			//bot.sendMessage(msg.chat.id, mex);
-
-			if (mex.type == "text") {
-				bot.sendMessage(msg.chat.id, mex.reply, { "parse_mode": "HTML" });
-			} else if (mex.type == "response") {
-				bot.sendMessage(msg.chat.id, mex.reply, {
-					"reply_to_message_id": msg.message_id,
-					"parse_mode": "HTML"
-				});
-			} else if (mex.type == "image") {
-				if (mex.reply.includes("gif")) {
-					bot.sendVideo(msg.chat.id, mex.reply);
-				} else {
-					bot.sendPhoto(msg.chat.id, mex.reply);
-				}
-			} else if (mex.type == "audio") {
-				bot.sendVoice(msg.chat.id, mex.reply);
-			} else if (mex.type == "function") {
-				//mex.reply(bot); //Future development
-			}
-		}
+          const chatId = msg.chat.id;
+          bot.sendPhoto(chatId, "./media/images/" + msg.text + ".jpg");
 	}
 });
-
-function controlMessage(message) {
-
-	let found = null;
-	//TODO: Should substitute forEach with for (const [triggers, oneFastAnswer] of Object.entries(fastAnswers)), in order to use return inside the loop
-	fastAnswers.forEach(function (fastAnswer) { //For every fast answer
-		fastAnswer.triggers.forEach(function (trigger) { //Check among all triggers
-			let regex = new RegExp("\\b" + trigger + "\\b", "gi");//Search global and case insenstive
-			let regexResult = message.match(regex);
-
-			//console.log("Regex result: " + regexResult);
-
-			if ((regexResult != null) && !found) { //If RegEx matches and wasn't previously found
-				let rnd = Math.floor((Math.random() * (fastAnswer.replies.length)) + 0);
-				found = fastAnswer.replies[rnd];
-				//return fastAnswer.replies[rnd]; //Can't do this with forEach (ahw man, that sucks), see comment above, substitute forEach with for
-			}
-		});
-	});
-
-	return found;
-}
